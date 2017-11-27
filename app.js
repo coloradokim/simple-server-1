d979442e6265691b85a9eff4b285fdd2dec30569
coloradokim <schlesinger.kim@gmail.com>
2017-11-27 11:04:39 -0700

    add dynamic port

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (req, res) => res.json({data: cohorts}))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
